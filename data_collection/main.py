87073ec72865ed8c2ced9f01f3f4b4bbe85da105
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-02-26 02:15:48 -0700

    Open and read csv file

--- data_collection/main.py
+++ data_collection/main.py
@@ -7,10 +7,14 @@ __author__ = "Ojas Chaturvedi"
 __github__ = "ojas-chaturvedi"
 __license__ = "MIT"
 
+import csv
 from web_scraper import web_scraper
 
 def main():
-    print("Hello World!")
+    # Open and read csv file
+    file = open("URL_links.csv")
+    reader = csv.reader(file)
+
 
 
 if __name__ == "__main__":
