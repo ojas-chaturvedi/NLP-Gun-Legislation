bf308cc3a841cd6bc8abcc82fc83839d5a03560c
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-02-26 01:53:59 -0700

    Create main.py

---
+++ data_collection/main.py
@@ -0,0 +1,17 @@
+#!/opt/homebrew/bin/python3
+"""
+File name / purpose
+"""
+
+__author__ = "Ojas Chaturvedi"
+__github__ = "ojas-chaturvedi"
+__license__ = "MIT"
+
+from web_scraper import main
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
