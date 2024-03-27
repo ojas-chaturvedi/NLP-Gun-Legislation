d821611e815a13dee3ad47cf28b0c6e753017a03
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-03-26 22:18:42 -0700

    Created testing function for classifier

--- data_collection/classification.py
+++ data_collection/classification.py
@@ -11,6 +11,7 @@ __license__ = "MIT"
 from os import getenv
 from dotenv import load_dotenv
 from openai import OpenAI
+from json import load
 
 
 def classification(legislative_text: str) -> str:
@@ -38,7 +39,18 @@ def classification(legislative_text: str) -> str:
     return response.choices[0].message.content
 
 
+def testing_classifier() -> None:
+    with open('data_collection/data/117.json', 'r') as f:
+        legislative_texts = load(f)
+    
+    for legislation in legislative_texts["Democratic"]:
+        # print(legislation.text)
+        print(f"Legislation {legislation["name"]}: " + classification(legislation["text"]))
+
+
 if __name__ == "__main__":
     # Test text
     text = "To prohibit Federal funding of State firearm ownership databases, and for other purposes. Be it enacted by the Senate and House of Representatives of the United States of America in Congress assembled, SECTION 1. SHORT TITLE. This Act may be cited as the ``Gun Owner Registration Information Protection Act''. SEC. 2. PROHIBITION ON FEDERAL FUNDING OF STATE FIREARM OWNERSHIP DATABASES. (a) Definitions.--In this section: (1) State.--The term ``State'' means each of the several States, the District of Columbia, the Commonwealth of Puerto Rico, the Commonwealth of the Northern Mariana Islands, American Samoa, Guam, the United States Virgin Islands, and any other territory or possession of the United States. (2) State firearm ownership database.--The term ``State firearm ownership database'' means a comprehensive or partial database of a State or political subdivision of a State that lists-- (A) firearms lawfully owned or possessed by individuals; or (B) individuals who lawfully own or possess firearms. (b) Prohibition.--A Federal agency may not fund or otherwise support the establishment or maintenance of a State firearm ownership database. (c) Exception.--Subsection (b) shall not apply to a database of a State or political subdivision of a State that lists-- (1) firearms that have been reported as lost or stolen; or (2) individuals who have reported their firearms as lost or stolen."
-    print(classification(text))
+    # print(classification(text))
+
+    testing_classifier()
