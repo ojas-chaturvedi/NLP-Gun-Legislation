0838d9313c6182de506ada23cc5441dae19fc16a
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-04-04 19:11:57 -0700

    Create sentiment_analysis/main.py

---
+++ sentiment_analysis/main.py
@@ -0,0 +1,20 @@
+#!/opt/homebrew/bin/python3
+"""
+Name: sentiment_analysis/main.py
+Purpose: To run the sentiment analysis models and generate results / graphs
+"""
+
+__author__ = "Ojas Chaturvedi"
+__github__ = "github.com/ojas-chaturvedi"
+__license__ = "MIT"
+
+from json import load
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+
+def main() -> None:
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
