0920ef970cc148a8e899193bb3e3d53d527cde72
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-04-14 21:54:23 -0700

    Added data for pro-control and pro-rights histogram

--- graphs/sentiments.py
+++ graphs/sentiments.py
@@ -15,6 +15,8 @@ from datetime import datetime
 from sentiment_analysis.initialize import session_end, session_start
 
 sentimentVStime = {}
+control = []
+rights = []
 
 for session in range(session_start, session_end + 1):
     csvFile = DictReader(open(f"sentiment_analysis/data/{session}.csv", "r"))
@@ -24,6 +26,11 @@ for session in range(session_start, session_end + 1):
         sentiment = float(legislation["VADER Sentiment Score"])
 
         sentimentVStime[date] = sentiment
+
+        if legislation["Classification"] == "control":
+            control.append(sentiment)
+        else:
+            rights.append(sentiment)
 
 sentimentVStime = dict(sorted(sentimentVStime.items()))
 
@@ -40,4 +47,26 @@ with open("graphs/overall.csv", "w") as file:
     for date, sentiment in sentimentVStime.items():
         data = [date, sentiment]
 
+        write.writerow(data)
+
+with open("graphs/control_histogram.csv", "w") as file:
+    write = writer(file)
+
+    headers = ["Pro-Control Legislation Sentiment Values"]
+
+    write.writerow(headers)
+
+    for value in control:
+        data = [value]
+        write.writerow(data)
+
+with open("graphs/rights_histogram.csv", "w") as file:
+    write = writer(file)
+
+    headers = ["Pro-Rights Legislation Sentiment Values"]
+
+    write.writerow(headers)
+
+    for value in rights:
+        data = [value]
         write.writerow(data)
