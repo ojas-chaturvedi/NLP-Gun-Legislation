4f323d2157ff12199322d11cf165150f5b1bf013
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-05-27 21:32:48 -0700

    Rename threads_api folder

---
+++ src/threads_api/__init__.py
@@ -0,0 +1,2 @@
+def hello() -> str:
+    return "Hello! This is the initialization file for the threads_api submodule."
---
+++ src/threads_api/main.py
@@ -0,0 +1,17 @@
+#!/opt/homebrew/bin/python3
+"""
+Name: src/threads/main.py
+Purpose: To utilize the Threads Python API and retrieve posts related to gun control
+"""
+
+__author__ = "Ojas Chaturvedi"
+__github__ = "github.com/ojas-chaturvedi"
+__license__ = "MIT"
+
+
+def main() -> None:
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
